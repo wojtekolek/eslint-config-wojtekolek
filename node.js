48c16fc06742610ef64f0bebc038bf2fba191509
wojtekolek <wojtek_olek@icloud.com>
2021-11-04 12:36:54 +0100

    Fix missing plugin issue

--- node.js
+++ node.js
@@ -1,3 +1,11 @@
+/*
+ * @rushstack/eslint-patch is used to include plugins as dev
+ * dependencies instead of imposing them as peer dependencies
+ *
+ * https://www.npmjs.com/package/@rushstack/eslint-patch
+ */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 const {
   warning,
   off,
