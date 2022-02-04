fbe6588016003b08c5c53dc9bb0b45ccfa9581f6
wojtekolek <wojtek_olek@icloud.com>
2022-02-04 09:53:31 +0100

    Add import order prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,15 @@ module.exports = {
   singleQuote: true,
   tabWidth: 2,
   trailingComma: "none",
+  importOrder: [
+    '^react$',
+    '<THIRD_PARTY_MODULES>',
+    '^@(pollen|ui|verve|verveLegacy)/(.*)$',
+    '^@(.*)/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
   overrides: [
     {
       files: "package*.json",
