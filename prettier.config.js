5287d240d69145c700b44792e50fca8ca057ee64
wojtekolek <wojtek_olek@icloud.com>
2022-02-04 09:56:40 +0100

    Improve config and bump deps

--- prettier.config.js
+++ prettier.config.js
@@ -4,22 +4,16 @@ module.exports = {
   semi: false,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: "none",
-  importOrder: [
-    '^react$',
-    '<THIRD_PARTY_MODULES>',
-    '^@(pollen|ui|verve|verveLegacy)/(.*)$',
-    '^@(.*)/(.*)$',
-    '^[./]',
-  ],
+  trailingComma: 'none',
+  importOrder: ['^react$', '<THIRD_PARTY_MODULES>', '^@(.*)/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   overrides: [
     {
-      files: "package*.json",
+      files: 'package*.json',
       options: {
-        printWidth: 1000,
-      },
-    },
-  ],
-};
+        printWidth: 1000
+      }
+    }
+  ]
+}
